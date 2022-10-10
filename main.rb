a62f76657563f15e39e6d19a50a7e28f34fcde9a
Julian Canepa <julian.l.canepa@gmail.com>
2022-10-10 11:06:47 -0700

    Refactor student class to adhere to the law of demeter, extracted into registrar class.

--- main.rb
+++ main.rb
@@ -1,18 +1,27 @@
+class Registrar
+  def remove_student_from_schedule(student, schedule, quarter_id)
+    quarter = schedule.quarter[quarter_id]
+    course_list = quarter.course_list
+    student.remove_from_schedule(course_list)
+  end
+
+  def add_student_to_schedule(student, schedule, quarter_id)
+    quarter = schedule.quarter[quarter_id]
+    course_list = quarter.course_list
+    student.add_to_schedule(course_list)
+  end
+end
+
 class Student
 
   MAX_COURSES = 5
 
-  def remove_from_schedule(course, schedule, quarter_id)
-    schedule.quarter[quarter_id].course_list.remove(course)
+  def remove_from_schedule(course_list)
+    course_list.remove(course)
   end
 
-  def add_to_schedule(course, schedule, quarter_id)
-    quarter = schedule.quarter[quarter_id]
-    course_list = quarter.course_list
+  def add_to_schedule(course_list)
     max_courses = course_list.maximum_number_of_courses
-
-    if max_courses < MAX_COURSES
-      course_list.add(course)
-    end
+    course_list.add(course) if max_courses < MAX_COURSES
   end
 end
