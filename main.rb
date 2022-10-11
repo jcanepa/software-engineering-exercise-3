ca1cdcffb83a1969e64d9c4683640e611869927a
Julian Canepa <julian.l.canepa@gmail.com>
2022-10-10 22:08:21 -0700

    Refactor to two classes. Extracting deep method chaining.

--- main.rb
+++ main.rb
@@ -1,20 +1,34 @@
-class Registrar
+class ScheduleReference
+  attr_reader :schedule
 
-  MAX_COURSES = 5
-
-  def get_course_list(schedule, quarter_id)
-    quarter = schedule.quarter[quarter_id]
-    return quarter.course_list
+  def initialize(schedule)
+    @schedule = schedule
   end
 
+  def get_quarter(quarter_id)
+    return @schedule.quarter[quarter_id]
+  end
+end
+
+class Registrar
+  MAX_COURSES = 5
+  attr_reader :course_list
+
+  def initialize(reference, quarter_id)
+    quarter = reference.get_quarter(quarter_id)
+		@course_list = quarter.course_list
+	end
+
   def remove_from_schedule(course)
-    get_course_list().remove(
+    @course_list.remove(
       course)
   end
 
   def add_to_schedule(course)
-    course_list = get_course_list()
-    max_courses = course_list.maximum_number_of_courses
-    course_list.add(course) if max_courses < MAX_COURSES
+    @course_list.add(course) if course_list_max() < MAX_COURSES
+  end
+
+  def course_list_max
+    return @course_list.maximum_number_of_courses
   end
 end
