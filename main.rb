a6f10f6a0a4aa29cc6b33f9c8891b605727f7490
Julian Canepa <julian.l.canepa@gmail.com>
2022-10-10 21:20:15 -0700

    Add proper indentation and give the class a more appropriate name, it has nothing to do with a Stundent or their concerns.

--- main.rb
+++ main.rb
@@ -1,13 +1,13 @@
-class Student
+class Registrar
 
   MAX_COURSES = 5
 
   def remove_from_schedule(course, schedule, quarter_id)
-  schedule.quarter[quarter_id].course_list.remove(course)
+    schedule.quarter[quarter_id].course_list.remove(course)
   end
 
   def add_to_schedule(course, schedule, quarter_id)
-    quarter     = schedule.quarter[quarter_id]
+    quarter = schedule.quarter[quarter_id]
     course_list = quarter.course_list
     max_courses = course_list.maximum_number_of_courses
 
