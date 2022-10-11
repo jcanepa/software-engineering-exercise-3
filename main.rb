8b6b1bf752790dcbb0770e92198a62a07b77b7ea
Julian Canepa <julian.l.canepa@gmail.com>
2022-10-10 21:17:49 -0700

    Commit mess before starting from scratch.

--- main.rb
+++ main.rb
@@ -4,26 +4,25 @@ class Registrar
     return quarter.course_list
   end
 
-  def remove_student_from_schedule(student, schedule, quarter_id)
-    course_list = get_course_list(schedule, quarter_id)
-    student.remove_from_schedule(course_list)
+  def remove_student_from_schedule(course_controller, schedule, quarter_id)
+    course_controller.remove_from_list(
+      get_course_list(schedule, quarter_id))
   end
 
-  def add_student_to_schedule(student, schedule, quarter_id)
-    course_list = get_course_list(schedule, quarter_id)
-    student.add_to_schedule(course_list)
+  def add_course_controller_to_schedule(course_controller, schedule, quarter_id)
+    course_controller.add_to_list(
+      get_course_list(schedule, quarter_id))
   end
 end
 
-class Student
-
+class CourseController
   MAX_COURSES = 5
 
-  def remove_from_schedule(course_list)
+  def remove_from_list(course, course_list)
     course_list.remove(course)
   end
 
-  def add_to_schedule(course_list)
+  def add_to_list(course, course_list)
     max_courses = course_list.maximum_number_of_courses
     course_list.add(course) if max_courses < MAX_COURSES
   end
