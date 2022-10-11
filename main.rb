50ab74ca26d1092f9f2cca7ffd756b47468118da
Julian Canepa <julian.l.canepa@gmail.com>
2022-10-10 21:22:15 -0700

    Follow uniform vairable extractions, prepare to extract duplicate code.

--- main.rb
+++ main.rb
@@ -3,7 +3,9 @@ class Registrar
   MAX_COURSES = 5
 
   def remove_from_schedule(course, schedule, quarter_id)
-    schedule.quarter[quarter_id].course_list.remove(course)
+    quarter = schedule.quarter[quarter_id]
+    course_list = quarter.course_list
+    course_list.remove(course)
   end
 
   def add_to_schedule(course, schedule, quarter_id)
