4fd5aea6631103bf089226544c564b95f1c5973b
Julian Canepa <julian.l.canepa@gmail.com>
2022-10-10 21:18:56 -0700

    Start over, with proper spacing.

--- main.rb
+++ main.rb
@@ -1,29 +1,18 @@
-class Registrar
-  def get_course_list(schedule, quarter_id)
-    quarter = schedule.quarter[quarter_id]
-    return quarter.course_list
-  end
+class Student
 
-  def remove_student_from_schedule(course_controller, schedule, quarter_id)
-    course_controller.remove_from_list(
-      get_course_list(schedule, quarter_id))
-  end
-
-  def add_course_controller_to_schedule(course_controller, schedule, quarter_id)
-    course_controller.add_to_list(
-      get_course_list(schedule, quarter_id))
-  end
-end
-
-class CourseController
   MAX_COURSES = 5
 
-  def remove_from_list(course, course_list)
-    course_list.remove(course)
+  def remove_from_schedule(course, schedule, quarter_id)
+  schedule.quarter[quarter_id].course_list.remove(course)
   end
 
-  def add_to_list(course, course_list)
+  def add_to_schedule(course, schedule, quarter_id)
+    quarter     = schedule.quarter[quarter_id]
+    course_list = quarter.course_list
     max_courses = course_list.maximum_number_of_courses
-    course_list.add(course) if max_courses < MAX_COURSES
+
+    if max_courses < MAX_COURSES
+      course_list.add(course)
+    end
   end
 end
